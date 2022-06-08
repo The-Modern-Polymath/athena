f9fbbdefa3eb0326db107778df9e645da0abd54c
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-06-08 15:28:27 -0700

    chore: fix ESLint settings, update index.ts to match rules

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
-import DiscordJS, { Intents } from 'discord.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import { Client, Intents } from 'discord.js';
+import 'dotenv/config';
 
-const client = new DiscordJS.Client({
-    intents:[
+const client = new Client({
+    intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
-})
+});
 
 client.on('ready', () => {
     console.log('Hi bob! The bot is ready!');
-})
+});
 
-client.on('messageCreate', (message) => {
-    if(message.content === 'ping'){
-        message.reply({
+client.on('messageCreate', async (message) => {
+    if (message.content === 'ping') {
+        await message.reply({
             content: 'pong',
-        })
+        });
     }
-})
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN).catch((err) => {
+    console.log(err);
+});
